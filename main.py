d8f9c16aa20ef5ce34b20313993b53cd220473ae
Galladian <Drew@sangfamily.com>
2026-06-17 15:33:34 +1200

    providing backend variables to entry fields

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import customtkinter as ctk
 from ctypes import windll, byref, sizeof, c_int
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import quirks
 
 BACKGROUND_COLOR = "#19233c"
 #endregion 
@@ -16,6 +17,12 @@ class App(ctk.CTk):
         self.title(" Compound interest calculator")
         self.ChangeTitleBar()
 
+        # data
+        self.initial_investment = ctk.StringVar(value = "0")
+        self.monthly_contribution = ctk.StringVar(value = "0")
+        self.years_invested = ctk.StringVar(value = "0")
+        self.interest_rate = ctk.StringVar(value = "0")
+
         # function
         self.CreateFrames()
 
@@ -24,7 +31,12 @@ class App(ctk.CTk):
     
     def CreateFrames(self) -> None:
         '''Creates frames for the app'''
-        self.input_frame = InputFrame(self)
+        self.input_frame = InputFrame(
+            self, 
+            self.initial_investment, 
+            self.monthly_contribution, 
+            self.years_invested, 
+            self.interest_rate)
         self.input_frame.place(relx = 0, rely = 0, relwidth = 0.4, relheight = 1)
         self.visual_frame = VisualFrame(self)
         self.visual_frame.place(relx = 0.4, rely = 0, relwidth = 0.6, relheight = 1)
@@ -48,17 +60,21 @@ class App(ctk.CTk):
 
 #region input frame
 class InputFrame(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):
+    def __init__(self, master, initial_investment: str, monthly_contribution: str, 
+                 years_invested: str, interest_rate: str, **kwargs):
+        # frame setup
         super().__init__(master, corner_radius = 0, fg_color = BACKGROUND_COLOR, **kwargs)
-
         self.rowconfigure((0, 1, 2, 3, 4), weight = 1, uniform = "a")
         self.columnconfigure(0, weight = 3, uniform = "a")
         self.columnconfigure(1, weight = 2, uniform = "a")
 
-        self.InitialWidgets()
-        self.ContributionWidgets()
-        self.TimeWidgets()
-        self.InterestWidgets()
+        self.vcmd = (self.register(quirks.CheckIfFloat), '%P')
+
+        # call widget functions
+        self.InitialWidgets(initial_investment)
+        self.ContributionWidgets(monthly_contribution)
+        self.TimeWidgets(years_invested)
+        self.InterestWidgets(interest_rate)
 
         # need to program function
         self.calculate_button = ctk.CTkButton(
@@ -68,7 +84,8 @@ class InputFrame(ctk.CTkFrame):
         )
         self.calculate_button.grid(row = 4, column = 0, columnspan = 2, pady = (10, 0))
 
-    def InitialWidgets(self) -> None:
+    def InitialWidgets(self, initial_investment: str) -> None:
+        '''Creates widgets for initial investment input'''
         self.initial_label = ctk.CTkLabel(
             self,
             text = "Initial Investment $",
@@ -82,11 +99,15 @@ class InputFrame(ctk.CTkFrame):
             font = ("Helvetica", 15),
             width = 100,
             height = 30,
-            state = "normal"
+            state = "normal",
+            textvariable = initial_investment,
+            validate = "key",         
+            validatecommand=self.vcmd
         )
         self.initial_entry.grid(row = 0, column = 1, sticky = "w", padx = (5, 0))
 
-    def ContributionWidgets(self) -> None:
+    def ContributionWidgets(self, monthly_contribution: str) -> None:
+        '''Creates widgets for monthly contribution input'''
         self.weeklyContribution_label = ctk.CTkLabel(
             self,
             text = "Weekly contribution $",
@@ -100,11 +121,15 @@ class InputFrame(ctk.CTkFrame):
             font = ("Helvetica", 15),
             width = 100,
             height = 30,
-            state = "normal"
+            state = "normal",
+            textvariable = monthly_contribution,
+            validate = "key",         
+            validatecommand=self.vcmd
         )
         self.weeklyContribution_entry.grid(row = 1, column = 1, sticky = "w", padx = (5, 0))
 
-    def TimeWidgets(self) -> None:
+    def TimeWidgets(self, years_invested: str) -> None:
+        ''''Creates widgets for time invested input'''
         self.time_label = ctk.CTkLabel(
             self,
             text = "Years invested",
@@ -118,11 +143,15 @@ class InputFrame(ctk.CTkFrame):
             font = ("Helvetica", 15),
             width = 100,
             height = 30,
-            state = "normal"
+            state = "normal",
+            textvariable = years_invested,
+            validate = "key",         
+            validatecommand=self.vcmd
         )
         self.time_entry.grid(row = 2, column = 1, sticky = "w", padx = (5, 0))
 
-    def InterestWidgets(self) -> None:
+    def InterestWidgets(self, interest_rate: str) -> None:
+        '''Creates widgets for interest rate input'''
         self.interest_label = ctk.CTkLabel(
             self,
             text = "Est interest rate %",
@@ -136,7 +165,10 @@ class InputFrame(ctk.CTkFrame):
             font = ("Helvetica", 15),
             width = 100,
             height = 30,
-            state = "normal"
+            state = "normal",
+            textvariable = interest_rate,
+            validate = "key",         
+            validatecommand=self.vcmd
         )
         self.interest_entry.grid(row = 3, column = 1, sticky = "w", padx = (5, 0))
 #end region
@@ -158,7 +190,7 @@ class VisualFrame(ctk.CTkFrame):
 
         self.PlotEmptyGraph()
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
-        self.canvas.get_tk_widget().pack(fill="both", expand=True, padx=10, pady=10)
+        self.canvas.get_tk_widget().pack(fill = "both", expand = True, padx = 10, pady = 10)
 
     def PlotEmptyGraph(self) -> None:
         """Sets up a clean, empty placeholder state for the graph."""
---
+++ quirks.py
@@ -0,0 +1,66 @@
+import time
+import re
+
+#region Datatype Validator
+def CheckIfNumber(_stringVariable: str) -> bool:
+    '''Checks of a string variable is an number'''
+    try:
+        float(_stringVariable)
+        return True
+    except ValueError:
+        return False
+
+def CheckIfInt(_stringVariable: str) -> bool:
+    '''Checks of a string variable is an integer'''
+    try:
+        int(_stringVariable)
+        return True
+    except ValueError:
+        return False
+    
+def CheckIfFloat(_stringVariable: str) -> bool:
+    '''Allow decimals and fractional numbers'''
+    if _stringVariable in ("", ".", "-", "-.", "+", "+."):
+        return True
+    try:
+        float(_stringVariable)
+        return True
+    except ValueError:
+        return False
+    
+def CheckIfAlpha(_stringVariable: str) -> bool:
+    '''Allow letters only.'''
+    if _stringVariable == "":
+        return True
+    return _stringVariable.isalpha()
+#endregion
+
+#region Decorators
+def RuntimeDecorator(_function: any) -> None:
+    def Wrapper(*args, **kwargs):
+        '''Call as a decorator to check duration to run code'''
+        
+        startTime: int  = time.time()
+        result = _function(*args, **kwargs) # calls function allowing arguments passed
+        duration: int = time.time() - startTime
+
+        print(f"{_function.__name__}() took {duration:.2f} to complete.")
+        return result
+    return Wrapper
+#endregion
+
+#region String functions
+def DeleteLastWord(_stringVariable: str) -> str:
+    '''Removes the last entered word'''
+    pattern = r'(.*)\s+\S+\s*$'
+    result = re.sub(pattern, r'\1', _stringVariable)
+    return result
+
+def ValidateMaxLength(_string: str, _max: int = 10) -> bool:
+    '''If string character count is less than max specified return true'''
+    return len(_string) <= _max
+
+def SetMaxLength(_string: str, _max:int = 10) -> str:
+    '''Cuts any character after specified length'''
+    return _string[:_max]
+#endregion
